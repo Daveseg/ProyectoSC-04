b4d8ee3ea385a8ea8743946d9771d0f31fe61591
guzulman <123052175+guzulman@users.noreply.github.com>
2023-03-16 04:06:42 -0600

    otros cambios

--- Proyecto/src/main/java/Tarea1HTML/demo/controller/CookieController.java
+++ Proyecto/src/main/java/Tarea1HTML/demo/controller/CookieController.java
@@ -6,7 +6,7 @@ package Tarea1HTML.demo.controller;
 
 
 import Tarea1HTML.demo.entity.Cookie;
-import Tarea1HTML.demo.service.CookieService;
+
 import Tarea1HTML.demo.service.ICookieService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
