2dc30d905d89bcd2b417cdd63ce68ed69e306795
guzulman <123052175+guzulman@users.noreply.github.com>
2023-04-27 03:03:38 -0600

    Implementación Cambio de idioma

---
+++ Proyecto/src/main/resources/static/idioma.js
@@ -0,0 +1,74 @@
+       function traducirEsp() {
+      var texto = document.getElementById("aboutUs");
+      texto.innerHTML = "About us";
+      
+      var texto = document.getElementById("Menu");
+      texto.innerHTML = "Menu";
+      
+      var texto = document.getElementById("Locations");
+      texto.innerHTML = "Locations";
+      
+      var texto = document.getElementById("Cart");
+      texto.innerHTML = "Cart";
+      
+      var texto = document.getElementById("OrderNow");
+      texto.innerHTML = "ORDER NOW";
+      
+      var texto = document.getElementById("h1");
+      texto.innerHTML = "Life is short,";
+      
+      var texto = document.getElementById("h12");
+      texto.innerHTML = "eat dessert first";
+      
+      var texto = document.getElementById("cookies");
+      texto.innerHTML = "Cookies";
+      
+      var texto = document.getElementById("loveem");
+      texto.innerHTML = "We all love them, right?";
+      
+      var texto = document.getElementById("eatone");
+      texto.innerHTML = "Just eat one.";
+      
+      var texto = document.getElementById("thankuslater");
+      texto.innerHTML = "You will thank us later.";
+      
+      var texto = document.getElementById("allLocations");
+      texto.innerHTML = "All Locations";
+    }
+    
+    function traducirIng() {
+      var texto = document.getElementById("aboutUs");
+      texto.innerHTML = "Nosotros";
+      
+      var texto = document.getElementById("Menu");
+      texto.innerHTML = "Men&uacute;";
+      
+      var texto = document.getElementById("Locations");
+      texto.innerHTML = "Ubicaciones";
+      
+      var texto = document.getElementById("Cart");
+      texto.innerHTML = "Carrito";
+      
+      var texto = document.getElementById("OrderNow");
+      texto.innerHTML = "ORDENA AHORA";
+      var texto = document.getElementById("h1");
+      texto.innerHTML = "La vida es corta,";
+      
+      var texto = document.getElementById("h12");
+      texto.innerHTML = "c&oacutemete un postre";
+      
+      var texto = document.getElementById("cookies");
+      texto.innerHTML = "Galletas";
+      
+      var texto = document.getElementById("loveem");
+      texto.innerHTML = "Todos las amamos, no es as&iacute?";
+      
+      var texto = document.getElementById("eatone");
+      texto.innerHTML = "Come una.";
+      
+      var texto = document.getElementById("thankuslater");
+      texto.innerHTML = "Y despu&eacutes nos agradeces";
+      
+      var texto = document.getElementById("allLocations");
+      texto.innerHTML = "Todas las ubicaciones";
+    }
